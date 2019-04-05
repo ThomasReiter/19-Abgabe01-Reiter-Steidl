d61fb428e078f8c37415653806e65a0208d53507
Reiter Thomas <thomas.reiterh-joanneum.at>
2019-04-05 14:14:31 +0200

    visual update by commenting program

--- versioning/GitCalculatorExercise/src/at/fhj/iit/Calculator.java
+++ versioning/GitCalculatorExercise/src/at/fhj/iit/Calculator.java
@@ -3,70 +3,15 @@ package at.fhj.iit;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// changed interface calculator to class calculator to work with
 public class Calculator {
 	
-	/**
-	 * Student 1
-	 * Student 2+
-	 * Student 3 (OPTIONAL)
-	 * 
-	 * TODO Instruction for GIT Lab for Grading GIT Part of Configuration Management
-	 *  - copy content of this git repository / zip file
-	 *
-	 *	- create your own remote repository, take care about Project Layout (!)
-	 * 
-	 *  (! Student 1 !)
-	 *  - create Main Class with 'public static void main(String[] args)'
-	 *	- create empty getMinimum() 
-	 *	- create empty getMaximum()
-	 *	- create addValue() which add an Integer to a list for later usage in getMinimum(), getMaximum() and other Methods
-	 *
-	 *  => publish current skeleton for your colleague(s) in remote repository
-	 *
-	 *
-	 *  (! Student 1 !)
-	 *  - implement getMinimum() which returns lowest Integer
-	 *	- test your method in main()
-	 * 
-	 *  => publish current implementation for your colleague(s) in remote repository
-	 *
-	 *
-	 *  (! Student 2 !)
-	 *  - implement getMaximum() which returns highest Integer
-	 *	- test your method in main()
-	 * 
-	 *  => publish current implementation for your colleague(s) in remote repository
-	 *  
-	 * 
-	 * (! all Students !)
-	 *  - implement a sum() which returns the sum of all Integer Values added by add()
-	 *	- implement this method with different implementations
-	 *	- commit your changes
-	 *	- if every Student has finished with commit, start to push your implementation
-	 *	- if every implementation has been pushed to remote, tag final implementation with "FinalVersion"
-	 *  
-	 *
-	 * FINAL ASSIGNMENT
-	 *  - don't forget to push everything (!) 
-	 *	  AND final version includes 
-	 *		- runnable source code
-	 *		- with test call methods in main Class
-	 *		- before commit take care that you previously tested your own source code 
-	 * - finish your assignment at elearning website with publishing repository url
-	 *
-	 * 
-	 * REMEMBER Git commands:
-	 *
-	 *   											<- Clone (FROM REMOTE)
-	 *   				<- Checkout (From LOCAL)
-	 *   -> (add) 		-> Commit (TO LOCAL) 		-> Push (TO REMOTE)
-	 *   					  		(LOCAL)			<- Pull (FROM REMOTE)
-	 *
-	 *
-	 */
-
+	
+	//Calculator list 
 	static List<Integer> testingList = new ArrayList<>();
 
+	//get minimum method to call in main and get the minimum Integer of the List
 	public int getMinimum() {
 		
 		int minimum = (int) testingList.get(0);
@@ -80,6 +25,7 @@ public class Calculator {
 		return minimum;
 	
 	}
+	//same as minimum but with the Maximum Integer
 	 public int getMaximum() {
 	    	
 	    	int maximum = (int) testingList.get(0);
@@ -93,6 +39,7 @@ public class Calculator {
 	    	return maximum;
 	   }
 	 
+	 //Method to calculate the sum of all Integers in the List by adding them up
 	 public int sum() {
 		int sum = 0;
 		
@@ -103,6 +50,7 @@ public class Calculator {
 		 
 	 }
 	 
+	 //Method to add an Integer to the List
 	  public List<Integer> addValue(int zugabe) {
 
 	        testingList.add(zugabe);
@@ -111,4 +59,63 @@ public class Calculator {
 
 
 }
+/**
+ * Student 1
+ * Student 2+
+ * Student 3 (OPTIONAL)
+ * 
+ * TODO Instruction for GIT Lab for Grading GIT Part of Configuration Management
+ *  - copy content of this git repository / zip file
+ *
+ *	- create your own remote repository, take care about Project Layout (!)
+ * 
+ *  (! Student 1 !)
+ *  - create Main Class with 'public static void main(String[] args)'
+ *	- create empty getMinimum() 
+ *	- create empty getMaximum()
+ *	- create addValue() which add an Integer to a list for later usage in getMinimum(), getMaximum() and other Methods
+ *
+ *  => publish current skeleton for your colleague(s) in remote repository
+ *
+ *
+ *  (! Student 1 !)
+ *  - implement getMinimum() which returns lowest Integer
+ *	- test your method in main()
+ * 
+ *  => publish current implementation for your colleague(s) in remote repository
+ *
+ *
+ *  (! Student 2 !)
+ *  - implement getMaximum() which returns highest Integer
+ *	- test your method in main()
+ * 
+ *  => publish current implementation for your colleague(s) in remote repository
+ *  
+ * 
+ * (! all Students !)
+ *  - implement a sum() which returns the sum of all Integer Values added by add()
+ *	- implement this method with different implementations
+ *	- commit your changes
+ *	- if every Student has finished with commit, start to push your implementation
+ *	- if every implementation has been pushed to remote, tag final implementation with "FinalVersion"
+ *  
+ *
+ * FINAL ASSIGNMENT
+ *  - don't forget to push everything (!) 
+ *	  AND final version includes 
+ *		- runnable source code
+ *		- with test call methods in main Class
+ *		- before commit take care that you previously tested your own source code 
+ * - finish your assignment at elearning website with publishing repository url
+ *
+ * 
+ * REMEMBER Git commands:
+ *
+ *   											<- Clone (FROM REMOTE)
+ *   				<- Checkout (From LOCAL)
+ *   -> (add) 		-> Commit (TO LOCAL) 		-> Push (TO REMOTE)
+ *   					  		(LOCAL)			<- Pull (FROM REMOTE)
+ *
+ *
+ */
 
--- versioning/GitCalculatorExercise/src/at/fhj/iit/Main.java
+++ versioning/GitCalculatorExercise/src/at/fhj/iit/Main.java
@@ -1,23 +1,23 @@
 package at.fhj.iit;
-
+//Main class with all the testcalls 
 public class Main {
 
 	public static void main(String[] args) {
-
+		//creating the calculator list which we work with
 		Calculator calc = new Calculator();
 		
-		
+		//first added Integers
 		calc.addValue(20);	
 		calc.addValue(4);	
 		calc.addValue(16);
 
-
+		//Integers we added to the list
 		calc.addValue(23);	
 		calc.addValue(33);	
-		calc.addValue(1);
+		calc.addValue(1); //lowest Integer (should be)
 		calc.addValue(23);	
-		calc.addValue(100);	
-		calc.addValue(16);
+		calc.addValue(100);	//highest Integer (should be)
+		calc.addValue(16); // use a Integer twice to show that the Methods still work with multible Integers with the same Value
 	
 		
 		// output lowest number 
