e8c500afa8f29b5a8ef3549ac09c8a87a7c0cd88
Maximilian Steidl <maximilian.steidl@edu.fh-joanneum.at>
2019-04-05 13:31:40 +0200

    added output for sum() and fixed some bugs

--- versioning/GitCalculatorExercise/src/at/fhj/iit/Main.java
+++ versioning/GitCalculatorExercise/src/at/fhj/iit/Main.java
@@ -1,8 +1,5 @@
 package at.fhj.iit;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -13,24 +10,21 @@ public class Main {
 		calc.addValue(20);	
 		calc.addValue(4);	
 		calc.addValue(16);
-		System.out.println(calc.getMaximum());
-		System.out.println(calc.getMinimum());
-		
-		
+
+
 		calc.addValue(23);	
 		calc.addValue(33);	
 		calc.addValue(1);
 		calc.addValue(23);	
 		calc.addValue(100);	
 		calc.addValue(16);
-		
-		System.out.println(calc.getMaximum());
-		System.out.println(calc.getMinimum());
+	
 		
 		// output lowest number 
-		
+		System.out.println(calc.getMinimum());
 		// output highest number
-		
+		System.out.println(calc.getMaximum());
 		// output sum of all numbers
+		System.out.println(calc.sum());
 	}
 }
